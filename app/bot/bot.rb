b25e92f13c3820dd79afae32ce4aa337d494a0bb
EC2 Default User <ec2-user@ip-172-31-37-209.us-east-2.compute.internal>
2018-12-27 00:38:18 +0000

    a little mistake fixed

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,5 +5,14 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV[ACCESS_TOKEN])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+  if message.text.include? "Bonjour"
+      message.reply(text: "Bonjour et bienvenu sur mon chatbot1 !")
+  elsif message.text.include? "idée"
+      message.reply(text: "Gratin dauphinois : un peu de crème, un peu de lait, et quelques pommes de terre")
+      message.reply(text: "Rien de plus simple pour étonner tes amis !")
+  elsif message.text.include? "Merci"
+      message.reply(text: "Avec plaisir ! N'hésite pas à venir me demander d'autres idées :)")
+  else 
+      message.reply(text: "Je ne comprends pas, par contre tu peux me demander de te donner une idée")
+  end
 end
